49866f861ea9f18241d151ab8acc93c0b461b7b9
javio127 <95726881+javio127@users.noreply.github.com>
2022-11-15 09:47:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,7 @@ import streamlit as st
 With the power of ML, we can help you find out if another profile is trustworthy or not below:
 """
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 trust_button = st.button("Investigate Profile!")
 st.write(trust_button)
